36ec17ffd21b55d705997201cbaeefaf86c25057
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2021-10-15 17:32:48 +0330

    Implemented Links Front Functionalities: - Added routes - Added List - Added Create - Added Edit and Delete

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -11,6 +11,10 @@ import AdminSnippetsList from "../views/admin/snippets/list";
 import AdminSnippetCreate from "../views/admin/snippets/create";
 import AdminSnippetEdit from "../views/admin/snippets/edit";
 
+import AdminLinksList from "../views/admin/links/list";
+import AdminLinkCreate from "../views/admin/links/create";
+import AdminLinkEdit from "../views/admin/links/edit";
+
 const routes = [
     {
         path: '',
@@ -34,6 +38,11 @@ const routes = [
             {path: 'snippets', component: AdminSnippetsList},
             {path: 'snippets/create', component: AdminSnippetCreate},
             {path: 'snippets/:id/edit', component: AdminSnippetEdit},
+
+            //link
+            {path: 'links', component: AdminLinksList},
+            {path: 'links/create', component: AdminLinkCreate},
+            {path: 'links/:id/edit', component: AdminLinkEdit},
         ]
     }
 ]
---
+++ resources/js/views/admin/links/create.vue
@@ -0,0 +1,122 @@
+<template>
+    <main class="flex-1 relative overflow-y-auto py-6 focus:outline-none" tabindex="0">
+        <form @submit.prevent="saveLink">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-5">
+                <div class="md:flex md:items-center md:justify-between">
+                    <div class="flex-1 min-w-0">
+                        <h1 class="py-0.5 text-2xl font-semibold text-gray-900">Create Link</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="mt-6 shadow sm:rounded-lg">
+                    <div class="bg-white md:grid md:grid-cols-3 md:gap-6 px-4 py-5 sm:p-6">
+                        <div class="md:col-span-1">
+                            <h3 class="text-lg font-medium leading-6 text-gray-900">Link</h3>
+                            <p class="mt-1 text-sm leading-5 text-gray-500">
+                                Link details.
+                            </p>
+                        </div>
+                        <div class="mt-5 md:mt-0 md:col-span-2">
+                            <div class="grid grid-cols-3 gap-6">
+
+                                <div class="col-span-3">
+                                    <label class="block text-sm font-medium leading-5 text-gray-700" for="title">Title</label>
+                                    <div class="mt-1 relative rounded-md shadow-sm">
+                                        <input
+                                            id="title"
+                                            v-model="link.title"
+                                            placeholder="Title"
+                                            class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                                            required
+                                        >
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="md:col-span-3">
+                                <div class="py-3">
+                                </div>
+                            </div>
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3">
+                                    <label class="block text-sm font-medium leading-5 text-gray-700" for="url">URL</label>
+                                    <div class="mt-1 relative rounded-md shadow-sm">
+                                        <input
+                                            id="url"
+                                            type="url"
+                                            v-model="link.url"
+                                            placeholder="URL"
+                                            class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                                            required
+                                        >
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="md:col-span-3">
+                                <div class="py-3">
+                                </div>
+                            </div>
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3">
+                                    <label class="text-sm font-medium leading-5 text-gray-700" for="open_in_new_tab">
+                                        <input
+                                            id="open_in_new_tab"
+                                            type="checkbox"
+                                            v-model="link.open_in_new_tab"
+                                        >
+                                        Open in new tab
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="bg-gray-100 text-right px-4 py-3 sm:px-6">
+                        <div class="inline-flex">
+                            <router-link
+                                class="btn btn-secondary shadow-sm rounded-md mr-4"
+                                to="/admin/links"
+                            >
+                                Cancel
+                            </router-link>
+                            <button
+                                class="btn btn-green shadow-sm rounded-md"
+                                type="submit"
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "new",
+    data() {
+        return {
+            link: {
+                title: null,
+                url: null,
+                open_in_new_tab: true,
+            },
+        }
+    },
+    methods: {
+        saveLink() {
+            const self = this;
+
+            //send snippet to server
+            axios.post('/api/admin/links', self.link).then(function (response) {
+                self.$router.push('/admin/links/');
+            }).catch(function () {
+                alert('Error creating resource');
+            });
+        }
+    },
+}
+</script>
