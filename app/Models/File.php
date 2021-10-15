1b23ad402328a3593dc24865da816531b29416e8
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2021-10-15 14:07:59 +0330

    Updated File Model Docs

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\File
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $disk
+ * @property string $path
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
 class File extends Model
 {
     use HasFactory;
