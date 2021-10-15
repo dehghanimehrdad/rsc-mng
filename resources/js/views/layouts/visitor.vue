8ece4fa6c3f1dcf2509a33b4d8075026a28e3c55
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2021-10-15 22:47:23 +0330

    Fixed overflow hidden problem

--- resources/js/views/layouts/visitor.vue
+++ resources/js/views/layouts/visitor.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="h-screen flex overflow-hidden bg-gray-100">
-        <div class="flex flex-col w-0 flex-1 overflow-hidden">
+    <div class="h-screen flex bg-gray-100">
+        <div class="flex flex-col w-0 flex-1">
             <div class="min-h-screen bg-gray-100">
                 <router-view></router-view>
             </div>
