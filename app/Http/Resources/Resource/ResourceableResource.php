ef25642079d4d9e6c7c0faa7f2797a3be1dae007
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2022-07-08 07:32:06 +0430

    updated ResourceableResource.php

--- app/Http/Resources/Resource/ResourceableResource.php
+++ app/Http/Resources/Resource/ResourceableResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Resource;
 use App\Models\File;
 use App\Models\Link;
 use App\Models\Resource;
+use App\Models\Snippet;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,8 +25,10 @@ class ResourceableResource extends JsonResource
             'id' => $resource->id,
             'title' => $resource->title,
             'description' => $resource->description,
-            'url' => $this->when(get_class($resource->resourceable) == File::class, $resource->resourceable->path),
+            'content' => $this->when(get_class($resource->resourceable) == Snippet::class, $resource->resourceable->content),
+            'file_url' => $this->when(get_class($resource->resourceable) == File::class, $resource->resourceable->path),
             'url' => $this->when(get_class($resource->resourceable) == Link::class, $resource->resourceable->url),
+            'open_in_new_tab' => $this->when(get_class($resource->resourceable) == Link::class, $resource->resourceable->open_in_new_tab),
             'type' => class_basename($resource->resourceable),
             'created_at' => $resource->created_at->toISOString(),
             'updated_at' => $resource->updated_at->toISOString(),
